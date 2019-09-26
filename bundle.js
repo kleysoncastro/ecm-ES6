cc86c4499e67446efbe6c43a61da4986b773887e
kleyson castro <kleysoncastro99@gamil.com>
2019-09-26 12:52:02 -0300

    conceito de arrow functions

--- bundle.js
+++ bundle.js
@@ -1,26 +1,9 @@
 "use strict";
 
-var nome = {
-  nome: 'kleyson'
-}; // reatribuicao de valor
+var list = [1, 2, 3, 4, 5]; // quando a funccao inter so tem uma instacao  a ser exutada
+// ela poder ser ecrita da forma abaixo
 
-nome.nome = 'castro';
-var list = [1, 2, 3, 4, 5, 6]; // map, 2 parametros, 1 o intem no array percorrido, 2 o indice.
-
-var novo = list.map(function (intem, index) {
-  return intem + index;
-}); // soma dos elementos de um retor
-
-var novo2 = list.reduce(function (total, prox) {
-  return total + prox;
-}); // filtro, funcao filter() retorma os valores que satisfazem a
-// condicao verdadeira da funcao interna
-
-var filter = list.filter(function (item) {
-  return item % 2 === 0;
-}); // procura item e o retorna o proprio elemento se encontrar
-
-var procura = list.find(function (intem) {
-  return intem === 10;
+var newList = list.map(function (item) {
+  return item * 2;
 });
-console.log(procura);
+console.log(newList);
