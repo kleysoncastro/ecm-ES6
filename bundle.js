cfb7069935bc3b23f0ea103ed44a927390f7ae34
kleyson castro <kleysoncastro99@gamil.com>
2019-09-26 13:22:41 -0300

    plugin para estudo conceito de rest/spread, @babel/plugin-proposal-object-rest-spread

--- bundle.js
+++ bundle.js
@@ -1,9 +1,1 @@
 "use strict";
-
-var list = [1, 2, 3, 4, 5]; // quando a funccao inter so tem uma instacao  a ser exutada
-// ela poder ser ecrita da forma abaixo
-
-var newList = list.map(function (item) {
-  return item * 2;
-});
-console.log(newList);
