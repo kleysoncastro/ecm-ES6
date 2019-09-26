331463fa035186d7cad418eb588c366ca6437998
kleyson castro <kleysoncastro99@gamil.com>
2019-09-26 12:07:57 -0300

    teste d funtos para vetor

--- bundle.js
+++ bundle.js
@@ -1,76 +1,26 @@
 "use strict";
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+var nome = {
+  nome: 'kleyson'
+}; // reatribuicao de valor
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+nome.nome = 'castro';
+var list = [1, 2, 3, 4, 5, 6]; // map, 2 parametros, 1 o intem no array percorrido, 2 o indice.
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+var novo = list.map(function (intem, index) {
+  return intem + index;
+}); // soma dos elementos de um retor
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+var novo2 = list.reduce(function (total, prox) {
+  return total + prox;
+}); // filtro, funcao filter() retorma os valores que satisfazem a
+// condicao verdadeira da funcao interna
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+var filter = list.filter(function (item) {
+  return item % 2 === 0;
+}); // procura item e o retorna o proprio elemento se encontrar
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-// class list criada
-var Data =
-/*#__PURE__*/
-function () {
-  function Data() {
-    _classCallCheck(this, Data);
-
-    this.data = [];
-  }
-
-  _createClass(Data, [{
-    key: "add",
-    value: function add(data) {
-      this.data.push(data);
-      console.log('novo elemento');
-    }
-  }]);
-
-  return Data;
-}(); // fim  da class Data <====
-
-
-var List =
-/*#__PURE__*/
-function (_Data) {
-  _inherits(List, _Data);
-
-  function List(nome) {
-    var _this;
-
-    _classCallCheck(this, List);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(List).call(this));
-    _this.nome = 'kleyson';
-    return _this;
-  }
-
-  _createClass(List, [{
-    key: "mostraNome",
-    value: function mostraNome() {
-      //this.nome.push(nome);
-      console.log();
-    }
-  }]);
-
-  return List;
-}(Data); // fim da class List   <====
-// Objeto instaciado da class List
-
-
-var novaLista = new List();
-
-document.getElementById('bt').onclick = function () {
-  novaLista.add();
-  novaLista.mostraNome('fram');
-};
+var procura = list.find(function (intem) {
+  return intem === 10;
+});
+console.log(procura);
--- main.js
+++ main.js
@@ -1,2 +1,30 @@
-const nu  =1;
-nu = 3;
+const nome = {nome: 'kleyson'}
+// reatribuicao de valor
+
+nome.nome = 'castro'
+
+const list = [1, 2, 3, 4, 5, 6];
+// map, 2 parametros, 1 o intem no array percorrido, 2 o indice.
+const novo = list.map(function(intem, index){
+        return intem + index;
+});
+
+// soma dos elementos de um retor
+
+const novo2 = list.reduce(function(total, prox){
+
+    return total + prox;
+});
+
+// filtro, funcao filter() retorma os valores que satisfazem a
+// condicao verdadeira da funcao interna
+const filter = list.filter(function(item){
+    return item % 2 === 0
+});
+
+// procura item e o retorna o proprio elemento se encontrar
+const procura = list.find(function(intem){
+
+    return intem === 10;
+});
+console.log(procura)
