0975d56652d28a4397da63b25e786591599a68b5
kleyson castro <kleysoncastro99@gamil.com>
2019-09-26 10:36:28 -0300

    criando class em nodejs, obj estrutura de class muito semelhante ao java

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert("teste");
+// class list criada
+class List{
+// costrutor da class, pode receber parametros
+    constructor() {
+        this.prod = [];
+    }
+// funcao da class, 
+    listTados() {
+        this.prod.push('novo elemento');
+        console.log(this.prod);
+    }
+} // fim da class List   <====
+
+// Objeto instaciado da class List
+const novaLista = new List();
+
+document.getElementById('bt').onclick = function() {
+    novaLista.listTados();
+};
